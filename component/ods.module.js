00e212498fdbbac05309779d3db5cc805c9a7376
Hermes Lorenzo <hlorenzo@tmunited.co>
2017-10-25 13:56:23 -0400

    Fixed Angular not using explicit annotation and cannot be invoked in strict mode.

--- component/ods.module.js
+++ component/ods.module.js
@@ -14,14 +14,19 @@ angular
         'dialogs.main',
         'dialogs.default-translations',
         'ngMessages',
-        'mgcrea.bootstrap.affix'
-    ], function ($rootScopeProvider) {
-
-        //We add this due to a limitation of AngularJS to avoid infinite recursion
-        // or infinite loop when dirty checking the model. In our case is because template recursion.
-        $rootScopeProvider.digestTtl(15);
-    })
+        'mgcrea.bootstrap.affix',
+        '$rootScopeProvider'
+    ], config)
     .value('version', '1.0');
+
+config.$inject = ['$rootScopeProvider'];
+
+function config($rootScopeProvider) {
+
+    //We add this due to a limitation of AngularJS to avoid infinite recursion
+    // or infinite loop when dirty checking the model. In our case is because template recursion.
+    $rootScopeProvider.digestTtl(15);
+}
 
 angular
     .module('ods-lib').run(function (editableOptions) {
