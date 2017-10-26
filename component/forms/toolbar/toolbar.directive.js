6826ed5da6e2a92c0ae545947e4abe709ef47383
Hermes Lorenzo <hlorenzo@tmunited.co>
2017-10-26 15:20:26 -0400

    Remove toolbar controller from form builder.

--- component/forms/toolbar/toolbar.directive.js
+++ component/forms/toolbar/toolbar.directive.js
@@ -7,18 +7,15 @@ angular
     .module('ods-lib')
     .directive('odsFormToolbar', OdsFormToolbar);
 
-OdsFormToolbar.$inject = ['OdsFormService', '$uibModal', '$sce', '$q'];
+OdsFormToolbar.$inject = ['OdsFormService'];
 
-function OdsFormToolbar(OdsFormService, $uibModal, $sce, $q) {
+function OdsFormToolbar(OdsFormService) {
 
     var directive = {
         restrict: 'E',
         templateUrl: 'forms/toolbar/toolbar.html',
-        // scope: {
-            // schema: '=',
-        // },
-        controller: 'OdsFormToolbarController',
-        controllerAs: 'vm',
+        // controller: 'OdsFormToolbarController',
+        // controllerAs: 'vm',
         link: linkFunc
     };
 
@@ -27,6 +24,78 @@ function OdsFormToolbar(OdsFormService, $uibModal, $sce, $q) {
     /* private helper methods*/
 
     function linkFunc($scope, $element) {
+
+        $scope.getToolbarComponent = getToolbarComponent;
+
+        $scope.toolbar = {
+            title: 'Fields Toolbar',
+            groups: [{
+                id: 0,
+                open: false,
+                disabled: false,
+                title: 'Layout',
+                icon: 'fa fa-dashboard',
+                components: [
+                    OdsFormService.newSectionObject()
+                ]
+            }, {
+                id: 1,
+                open: false,
+                disabled: false,
+                title: 'Text input fields',
+                icon: 'fa fa-dashboard',
+                components: [
+                    OdsFormService.newFieldTextObject(),
+                    OdsFormService.newFieldNumberObject(),
+                    OdsFormService.newFieldPasswordObject(),
+                    OdsFormService.newFieldTextareaObject()
+                ]
+            }, {
+                id: 2,
+                open: false,
+                disabled: false,
+                title: 'Select input fields',
+                icon: 'fa fa-dashboard',
+                components: [
+                    OdsFormService.newFieldSelectObject(),
+                    OdsFormService.newFieldMultiSelectObject()
+
+                ]
+            }, {
+                id: 3,
+                open: false,
+                disabled: false,
+                title: 'Toggle fields',
+                icon: 'fa fa-dashboard',
+                components: [
+                    OdsFormService.newFieldToggleObject()
+                ]
+            }, {
+                id: 4,
+                open: false,
+                disabled: false,
+                title: 'DateTime fields',
+                icon: 'fa fa-dashboard',
+                components: [
+                    OdsFormService.newDateTimeObject()
+                ]
+            }, {
+                id: 5,
+                open: false,
+                disabled: false,
+                title: 'Plugins',
+                icon: 'fa fa-dashboard',
+                components: [
+                    OdsFormService.newYesNoObject(),
+                    OdsFormService.newTableObject(),
+                    OdsFormService.newFieldLabelObject()
+                ]
+            }]
+        };
+
+        function getToolbarComponent(componentType) {
+            return OdsFormService.getToolbarComponent(componentType);
+        }
 
     }
 }
