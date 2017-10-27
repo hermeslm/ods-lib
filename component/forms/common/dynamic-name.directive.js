e2d3ca350214020ea4ac317aa0f016f7b9372676
Hermes Lorenzo <hlorenzo@tmunited.co>
2017-10-27 12:19:33 -0400

    Fixed form validations with some type of fields in schema definition time, remain ui-select field does not works.

---
+++ component/forms/common/dynamic-name.directive.js
@@ -0,0 +1,26 @@
+/**
+ * Created by hermeslm on 3/28/17.
+ */
+'use strict';
+
+angular
+    .module('ods-lib')
+    .directive('odsDynamicName', DynamicNameDirective);
+
+DynamicNameDirective.$inject = ['$compile', '$parse'];
+
+function DynamicNameDirective($compile, $parse) {
+
+    return {
+        restrict: 'A',
+        terminal: true,
+        priority: 100000,
+        link: function (scope, elem) {
+            var name = $parse(elem.attr('ods-dynamic-name'))(scope);
+            // $interpolate() will support things like 'skill'+skill.id where parse will not
+            elem.removeAttr('ods-dynamic-name');
+            elem.attr('name', name);
+            $compile(elem)(scope);
+        }
+    }
+}
