fd3a725a60996a3cffc0dabb6d0c4f76c165660d
hermeslm <hlorenzo@tmunited.co>
2018-04-21 21:34:36 -0400

    Fixed Address directive.

--- component/address/address-dialog.controller.js
+++ component/address/address-dialog.controller.js
@@ -7,14 +7,17 @@ angular
 AddressDialogController.$inject = ['$uibModalInstance', 'address', 'countries', 'states'];
 
 function AddressDialogController($uibModalInstance, address, countries, states) {
+
     var vm = this;
 
     vm.address = address;
     vm.countries = countries;
     vm.states = states;
+    vm.nonEmail = false;
 
     vm.clear = clear;
     vm.save = save;
+    vm.toggleEmail = toggleEmail;
 
     function clear() {
         $uibModalInstance.dismiss('cancel');
@@ -23,5 +26,12 @@ function AddressDialogController($uibModalInstance, address, countries, states) 
     function save() {
         $uibModalInstance.close(vm.address);
         vm.isSaving = false;
+    }
+
+    function toggleEmail() {
+
+        if (vm.nonEmail) {
+            vm.address.email = "non-email@domain.com";
+        }
     }
 }
