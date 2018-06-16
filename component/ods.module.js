432e3a3cece81766e4a7d2d3696c505cfd544b8c
hermeslm <hlorenzo@tmunited.co>
2018-06-16 05:39:26 -0400

    Fixed error in forms components with dialogs translations if language is present.

--- component/ods.module.js
+++ component/ods.module.js
@@ -32,12 +32,30 @@ angular
         // editableOptions.theme = 'bs3';
     });
 
-configFunction.$inject = ['$rootScopeProvider', '$localStorageProvider'];
+configFunction.$inject = ['$rootScopeProvider', '$localStorageProvider', '$translateProvider'];
 
-function configFunction($rootScopeProvider, $localStorageProvider) {
+function configFunction($rootScopeProvider, $localStorageProvider, $translateProvider) {
 
     //We add this due to a limitation of AngularJS to avoid infinite recursion
     // or infinite loop when dirty checking the model. In our case is because template recursion.
     $rootScopeProvider.digestTtl(15);
     $localStorageProvider.setKeyPrefix('ods-');
+
+    $translateProvider.translations('en-US', {
+        DIALOGS_ERROR: "Error",
+        DIALOGS_ERROR_MSG: "An unknown error has occurred.",
+        DIALOGS_CLOSE: "Close",
+        DIALOGS_PLEASE_WAIT: "Please Wait",
+        DIALOGS_PLEASE_WAIT_ELIPS: "Please Wait...",
+        DIALOGS_PLEASE_WAIT_MSG: "Waiting on operation to complete.",
+        DIALOGS_PERCENT_COMPLETE: "% Complete",
+        DIALOGS_NOTIFICATION: "Notification",
+        DIALOGS_NOTIFICATION_MSG: "Unknown application notification.",
+        DIALOGS_CONFIRMATION: "Confirmation",
+        DIALOGS_CONFIRMATION_MSG: "Confirmation required.",
+        DIALOGS_OK: "OK",
+        DIALOGS_YES: "Yes",
+        DIALOGS_NO: "No"
+    });
+
 }
